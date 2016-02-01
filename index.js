228056dd24c3dcaddf8316136e44de97dc25ba7a
Loïc Mahieu <mahieuloic@gmail.com>
2016-02-01 12:09:28 +0100

    Don't get container before upload. It is not necessary and for containers with lot of files, this can be really performance killer Bonus, reduce complexity

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function getDestination (req, file, cb) {
   cb(null, {
     container: 'uploads',
@@ -10,28 +11,12 @@ function PkgcloudStorage (opts) {
   this.getDestination = (opts.destination || getDestination)
 }
 
-function upload (req, file, that, params, cb) {
-  var outStream = that.client.upload(params)
-
-  file.stream.pipe(outStream)
-  outStream.on('error', cb)
-  outStream.on('success', function (info) {
-    cb(null, {
-      size: info.size,
-      container: params.container,
-      remote: params.remote
-    })
-  })
-}
-
 PkgcloudStorage.prototype._handleFile = function _handleFile (req, file, cb) {
   var that = this
 
   // Change destination if needed
   that.getDestination(req, file, function (err, dest) {
-    if (err) {
-      return cb(err)
-    }
+    if (err) return cb(err)
 
     var params = {
       container: dest.container,
@@ -39,24 +24,16 @@ PkgcloudStorage.prototype._handleFile = function _handleFile (req, file, cb) {
       contentType: file.mimetype
     }
 
-    // check if existence check is necessary
-    if (req.multerPkgcloudRecentContainer != null && req.multerPkgcloudRecentContainer.indexOf(params.container) >= 0) {
-      return upload(req, file, that, params, cb)
-    }
+    var outStream = that.client.upload(params)
 
-    // ensuring existence of container
-    that.client.getContainer(params.container, function (err, container) {
-      // need to check for an error and if the container is null (can really happen)
-      if (err != null) return cb(err)
-      if (container == null) return cb(new Error('Failed to ensure existence of container "' + params.container + '"'))
-
-      // storing of pkgcloud container object in req for processing afterwords
-      req.multerContainer = container
-      if (req.multerPkgcloudRecentContainer == null) {
-        req.multerPkgcloudRecentContainer = []
-      }
-      req.multerPkgcloudRecentContainer.push(params.container)
-      upload(req, file, that, params, cb)
+    file.stream.pipe(outStream)
+    outStream.on('error', cb)
+    outStream.on('success', function (info) {
+      cb(null, {
+        size: info.size,
+        container: params.container,
+        remote: params.remote
+      })
     })
   })
 }
